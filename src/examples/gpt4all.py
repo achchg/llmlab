dff81e5a5945eb46bfc9bca10af7555af1a53fc4
Chi-Hsuan Chang <achchg@gmail.com>
2024-03-18 17:22:28 -0400

    added examples for ollama

--- src/examples/gpt4all.py
+++ src/examples/gpt4all.py
@@ -1,10 +1,10 @@
-from langchain_community.llms import GPT4All
+from langchain_community.llms.gpt4all import GPT4All
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-local_path = 'path to where you save the model bin file'
-llm = GPT4All(model=local_path, callbacks=[StreamingStdOutCallbackHandler()])
+local_path = ('data/gpt4all-falcon-newbpe-q4_0.gguf.bin')
+llm = GPT4All(model=local_path)
 
 template = """{question}"""
 
