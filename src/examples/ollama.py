a78c92b4e7dcf70927176c936cd463dbfe3a133e
Chi-Hsuan Chang <achchg@gmail.com>
2024-06-17 17:26:39 -0400

    ollama

--- src/examples/ollama.py
+++ src/examples/ollama.py
@@ -1,11 +1,36 @@
 from langchain_community.llms import Ollama
 from langchain_community.chat_models import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
 from langchain_core.messages import HumanMessage
+from langchain_core.prompts import ChatPromptTemplate
 
-llm = Ollama(model="llama2")
-chat_model = ChatOllama()
+from langfuse import Langfuse
+from langfuse.callback import CallbackHandler
 
-text = "What would be a good time to eat lunch?"
-messages = [HumanMessage(content=text)]
+# llm = Ollama(model="llama2")
+llm = ChatOllama(model="llama2")
 
-print(llm.invoke(text))
+text = "You are a travel agent that help people prepare travel itinerary. {question}"
+prompt = ChatPromptTemplate.from_template(text)
+chain = prompt | llm | StrOutputParser()
+
+# messages = [HumanMessage(content=text)]
+# print(llm.invoke(text))
+
+langfuse = Langfuse()
+langfuse.create_prompt(
+    name="ollama-test-prompt",
+    prompt=text,
+    is_active=True,
+    config = {
+        "model": "llama2",
+        "temperature": 0.2,
+        "supported_languages": ["en"]
+    }
+)
+langfuse_handler = CallbackHandler(
+    session_id="test-1234",
+    user_id = "chi-local"
+)
+
+print(chain.invoke({"question": "Travel plan for 6 days Iceland travel in June."}, config={"callbacks": [langfuse_handler]}))
