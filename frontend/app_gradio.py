10dda2c9486a15b203f583266aaf23d15ab6751c
Chi-Hsuan Chang <achchg@gmail.com>
2024-07-16 17:18:33 -0400

    added comment for frontend:

--- frontend/app_gradio.py
+++ frontend/app_gradio.py
@@ -4,15 +4,12 @@ import gradio as gr
 
 from langchain.vectorstores.pgvector import PGVector
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
-import torch
-from threading import Thread
 import ollama
-import os
 
 
 from src.prompts.prompt_template import CVPrompt
 
-
+# use parser to pull model
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--model_name",
@@ -24,6 +21,7 @@ parser.add_argument(
 
 args = parser.parse_args()
 
+# connect to vectorstore
 CONNECTION_STRING=PGVector.connection_string_from_db_params(
         driver="psycopg2",
         host="localhost",
@@ -32,6 +30,7 @@ CONNECTION_STRING=PGVector.connection_string_from_db_params(
         user="langchain",
         password="langchain")
 
+# select the same embedding model that we used for vectorstore creation
 embedding_function = SentenceTransformerEmbeddings(
     model_name='all-MiniLM-L6-v2'
 )
@@ -42,22 +41,23 @@ db = PGVector(
     collection_name=collection_name,
     embedding_function=embedding_function)
 
+# set up gradio
 with gr.Blocks() as demo:
     gr.HTML(
         f"""
-        <h3> Model Name: {args.model_name}
-    """
+        <h3> Model Name: {args.model_name}</h3>"""
     )
 
-    chat_history = gr.Chatbot(label="QA Bot")
+    # create chatbot
+    chat_history = gr.Chatbot(label="QA bot for my portfolio")
     msg = gr.Textbox(label="Question", interactive=True)
 
-    with gr.Accordion(label="Generation Parameters", open = False):
+    # create interactive hyper-parameters
+    with gr.Accordion(label="LLM Hyper Parameters", open = False):
         prompt = CVPrompt()
         prompt_format = gr.Textbox(
             label="Formatting prompt",
-            value=f"""{prompt.template}
-""", interactive=True
+            value=f"""{prompt.template}""", interactive=True
         )
         with gr.Row():
             max_new_tokens = gr.Number(
@@ -76,13 +76,35 @@ with gr.Blocks() as demo:
 
 
     def user(user_message, history):
+        """
+        A function that handles user messages in a chatbot system.
+
+        Args:
+            user_message (str): The message sent by the user.
+            history (list): The history of the chat conversation.
+
+        Returns:
+            tuple: A tuple containing an empty string and the updated chat history.
+        """
+
         if history:
+            # Append the user's message to the history
             history.append([user_message, None])
             return "", history
         else:
             return "", [[user_message, None]]
     
     def format_history(history: list[list[str, str]], system_prompt: str):
+        """
+        Format the given chat history into a list of dictionaries representing each message in the conversation.
+
+        Args:
+            history (list[list[str, str]]): The chat history, where each sublist contains a query and its corresponding response.
+            system_prompt (str): The initial system prompt to be added to the chat history.
+
+        Returns:
+            list[dict]: The formatted chat history, where each dictionary represents a message and contains the role and content of the message.
+        """
         chat_history = [{"role": "system", "content": system_prompt}]
         print(f"history:{history}")
         for query, response in history:
@@ -90,29 +112,43 @@ with gr.Blocks() as demo:
             chat_history.append({"role": "assistant", "content": response})  
         return chat_history
 
-    def bot(chat_history, temperature, k=1):
+    def bot(chat_history, temperature, k, num_predict):
+        """
+        A function that processes chat history and generates a response using the Ollama chat model.
+        
+        Args:
+            chat_history (list): The history of the chat conversation.
+            temperature (float): The temperature parameter for response generation.
+            k (int): The number of similarity scores to consider.
+
+        Returns:
+            generator: A generator that yields the processed chat history.
+        """
+
         print(f"chat_history-start: {chat_history}")
         msg = chat_history[-1][0]
         similarity_scores = db.similarity_search_with_score(msg, k = k)
 
-        print(f"content-all: {similarity_scores}")
-        print(f"content: {similarity_scores[0][0].page_content}")
+        print(f"content-all: {[doc[0].page_content for doc in similarity_scores]}")
+        print(f"content-first: {similarity_scores[0][0].page_content}")
         print(f"similarity_score: {similarity_scores[0][1]}")
 
         formatted_prompt = prompt.template.format(
-            context = similarity_scores[0][0].page_content
+            context = ' \n ==='.join([doc[0].page_content for doc in similarity_scores])
         )
-        
+        print(f"formatted_prompt: {formatted_prompt}")
+
         chat_history = format_history(chat_history, formatted_prompt)
         print(f"chat_history: {chat_history}")
 
         generation_kwargs = dict(
             model=args.model_name,
-            stream=True,
-            temperature=temperature,
+            stream=False,
+            messages=chat_history,
+            options = {"temperature": temperature, "k": k},
         )
         
-        response = ollama.chat(model=args.model_name, stream=False, messages=chat_history)['message']['content']
+        response = ollama.chat(**generation_kwargs)['message']['content']
         print(response)
 
         print(f"chat_history - 0: {chat_history}")
@@ -133,7 +169,7 @@ with gr.Blocks() as demo:
     
 
     msg.submit(user, [msg, chat_history], [msg, chat_history], queue=False).then(
-        bot, [chat_history, temperature], chat_history
+        bot, [chat_history, temperature, k], chat_history
     )
 
 
