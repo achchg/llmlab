932bfe0e117317dcd2ce202832feca0eb8074b5f
Chi-Hsuan Chang <achchg@gmail.com>
2024-07-16 17:19:15 -0400

    added comment for prompt management

--- src/prompts/prompt_template.py
+++ src/prompts/prompt_template.py
@@ -5,8 +5,11 @@ from langchain.prompts import PromptTemplate
 __all__ = ["CVPrompt"]
 
 class CVPrompt:
+    """
+    CVPrompt class to manage the prompt template used for the CV usecase.
+    """
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.template = """You are an expert in talent acquisition. Please provide a summary of the profile in terms of the professional experience you received, highlighting key achievements and responsibilities. 
-        content: {context}
+        self.template = """You are a chatbot that supports talent acquisition. Please provide a summary of the profile in terms of the professional experience you received, highlighting key achievements and responsibilities. 
+        profile content: {context}
         """
